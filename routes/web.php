510179cbb1f6a26fb993f9ff88d4fb74527c168a
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-09-17 10:28:37 -0500

    API router - CREATED

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => config('app.name'),
+        'status' => 'ok',
+        'docs' => 'see README.md for the APi endpoints',
+    ]);
 });
